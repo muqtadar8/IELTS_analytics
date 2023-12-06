6259869b134a8ceefa414c84b621ef7753f83db5
muqtadar8 <66875673+muqtadar8@users.noreply.github.com>
2023-12-06 13:02:05 +0530

    Fixed Writing html

--- ielts/writing/views.py
+++ ielts/writing/views.py
@@ -1,12 +1,8 @@
-import openai 
+import openai
 from django.shortcuts import render
 from openai import OpenAI
 from django.conf import settings
-
 api_key = settings.OPENAI_API_KEY
-
-
-
 class ThreadMessage:
     def __init__(self, id, assistant_id, content, created_at, file_ids, metadata, object, role, run_id, thread_id):
         self.id = id
@@ -19,25 +15,22 @@ class ThreadMessage:
         self.role = role
         self.run_id = run_id
         self.thread_id = thread_id
-
 class MessageContentText:
     def __init__(self, text):
         self.text = text
-
 def display_assessment(assessment):
-    complete_text = ""
-
+    result = []
     for message in assessment:
         if message.role == 'assistant':
-            complete_text += "Assistant's Feedback:\n"
-            complete_text += message.content[0].text.value + '\n'
-        elif message.role == 'user':
-            complete_text += "User's Essay:\n"
-            complete_text += message.content[0].text.value + '\n' + '-'*50 + '\n'
-
-    return complete_text
-
-
+            content = message.content[0].text.value.replace('\n', '<br>').replace('**', '')
+            bold_phrases = ['Task Response','Task Achievement', 'Coherence and Cohesion', 'Lexical Resource', 'Grammatical Range and Accuracy', 'Overall Score', 'Feedback']
+            for phrase in bold_phrases:
+                content = content.replace(phrase, f'<strong>{phrase}</strong>')
+            result.append({
+                'role': "Assistant's Feedback",
+                'content': [content]
+            })
+    return result
 def feedback(inp):
     # client
     client = OpenAI(api_key=api_key)
@@ -67,7 +60,6 @@ def feedback(inp):
         assistant_id="asst_vKBqFPhSg7yKz4B1tHtrwXDG"
     )
     run_id  = run.id
-
     def rec():
       run = client.beta.threads.runs.retrieve(
       thread_id=th_id,
@@ -77,46 +69,14 @@ def feedback(inp):
         return
       else :
         rec()
-
     rec()
-
     thread_messages = client.beta.threads.messages.list(th_id)
     mes = thread_messages.data
-    print(type(mes))
-    
     client.beta.threads.delete(th_id)
-    
-    # return display_assessment(mes)
-    return mes
-
-def clean_text(raw_text):
-    # Check if the raw text is a string, if not, return an empty string
-    if isinstance(raw_text, str):
-        # Extract relevant part of the text
-        cleaned_text = raw_text.split(':', 1)[-1].strip()
-        return cleaned_text
-    else:
-        return ''
-
+    return display_assessment(mes)
 def main(request):
     result = None
-    cleaned_result = []  # Move this line outside the if block
     if request.method == 'POST':
         essay_input = request.POST['essay_input']
         result = feedback(essay_input)
-
-        # print("Result from feedback function:", result)  # Add this line
-
-        # # Clean the text in the result
-        # if result:
-        #     for message in result:
-        #         cleaned_message = {}
-        #         cleaned_message['role'] = message.role
-        #         cleaned_message['content'] = [
-        #             text_content.text
-        #             for text_content in message.content
-        #         ]
-        #         cleaned_result.append(cleaned_message)
-
-    # print("Cleaned Result:", cleaned_result)  # Add this line
     return render(request, 'index.html', {'result': result})
